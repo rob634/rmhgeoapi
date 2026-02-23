71c122a4d66d464fb1e210c7ec65c0711982751a
Robert Harrison <rob634@gmail.com>
2026-02-23 15:46:54 -0500

    V0.9.1.0 UI Updates

--- config/__init__.py
+++ config/__init__.py
@@ -45,7 +45,7 @@ Usage:
 # ============================================================================
 # Semantic versioning follows MAJOR.MINOR.PATCH.BUILD
 # V0.9 - Asset and Release data models and semantic versioning workflows
-__version__ = "0.9.0.0"
+__version__ = "0.9.1.0"
 
 from typing import Optional
 
